83d304d5e8da42a8158e3f0e6be473b4845f9f01
Sssubut <162115566+Sssubut@users.noreply.github.com>
2024-04-01 12:19:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ response = requests.get(api_url)
 if response.status_code == 200:
     # print(print(response.json()))
 
-    print(f'BTC to {response.json()['bpi'][f'{query}']['code']}')
-    print(f'Курс: {response.json()['bpi'][f'{query}']['rate']}')
+    print(f"BTC to {response.json()['bpi'][f"{query}"]['code']}")
+    print(f"Курс: {response.json()['bpi'][f"{query}"]['rate']}")
