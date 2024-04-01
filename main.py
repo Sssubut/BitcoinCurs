e2f44585f1e7926e73885206271987904b1dd20d
Sssubut <pomnim.skorbom@mail.ru>
2024-04-01 11:13:54 +0300

    Blabla

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import requests
 api_url = 'https://660a65c90f324a9a2884e818.mockapi.io/users'
 
 response = requests.get(api_url)
-
+#Проверка запроса
 if response.status_code == 200:
-    # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
     print(print(response.json()))
 else:
-    print(response.status_code)    # При другом коде ответа выводим этот код
+    print(response.status_code)
