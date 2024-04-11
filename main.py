a5f8f2c5bdb081813a9ef2b66e1b505129797a5d
Sssubut <162115566+Sssubut@users.noreply.github.com>
2024-04-11 11:06:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 
-query = input('(usd - 1 | gbp - 2 | eur - 3) > ').upper()
+query = input('(usd - 1 | gbp - 2 | eur - 3 | RUB - 4 | BYN - 5) > ').upper()
 
 if query == '1':
     query = 'usd'.upper()
@@ -8,6 +8,11 @@ elif query == '2':
     query = 'gbp'.upper()
 elif query == '3':
     query = 'eur'.upper()
+elif query == '4':
+    query = 'rub'.upper()
+elif query == '5':
+    query = 'byn'.upper()
+
 
 api_url = f'https://api.coindesk.com/v1/bpi/currentprice/{query}.json'
 response = requests.get(api_url)
